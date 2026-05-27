31eaabd68551a815f66dc2d2d2007728e42e01f3
Emipilica <capir.2025116isinf0031@aluno.ifpi.edu.br>
2026-05-27 16:54:56 -0300

    2. Alteração de Texto na Tela

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 function mostrarmensagem(){
-    alert("Bem vindo!")
+    ("Bem vindo!")
 }
+let p2 = "Cuidar da natureza é um dever de todos. Pequenas atitudes, como não jogar lixo no chão, economizar água e plantar árvores, ajudam a preservar o meio ambiente. Quando protegemos a natureza, garantimos um futuro melhor para as próximas gerações.";
