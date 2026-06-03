02b4eeb764e1e8dcbe02f7534097ed6cee58d494
Emipilica <capir.2025116isinf0031@aluno.ifpi.edu.br>
2026-06-03 07:50:57 -0300

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-function mostrarmensagem(){
-    ("Bem vindo!")
-}
-let p2 = "Cuidar da natureza é um dever de todos. Pequenas atitudes, como não jogar lixo no chão, economizar água e plantar árvores, ajudam a preservar o meio ambiente. Quando protegemos a natureza, garantimos um futuro melhor para as próximas gerações.";
